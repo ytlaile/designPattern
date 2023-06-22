9d09ff98dd103d00c6df1896d0ae2601e0d4a085
yt <1299949441@qq.com>
2023-06-22 10:22:57 +0800

    second commit

--- src/main/java/test/Builder.java
+++ src/main/java/test/Builder.java
@@ -5,11 +5,6 @@ import java.util.Arrays;
 public class Builder {
     public static void main(String[] args) {
         StringBuilder builder = new StringBuilder();   //创建一个StringBuilder来逐步构建一个字符串
-        builder.append(666);   //拼接一个数字
-        builder.append("老铁");   //拼接一个字符串
-        builder.insert(2, '?');  //在第三个位置插入一个字符
-        System.out.println(builder);   //差不多成形了，最后转换为字符串
 
-        System.out.println(Arrays.asList("da", "2"));
     }
 }
