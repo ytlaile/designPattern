b389e5a48e008800107c1402b091acdfe7cc2d85
yt <1299949441@qq.com>
2023-06-22 10:35:31 +0800

    hot commit

--- src/main/java/test/Builder.java
+++ src/main/java/test/Builder.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 
 public class Builder {
     public static void main(String[] args) {
-        StringBuilder builder = new StringBuilder();   //创建一个StringBuilder来逐步构建一个字符串
-
+        System.out.println("hot");
     }
 }
